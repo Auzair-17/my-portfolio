03af3ed66b2f93e005a570adbe6622d7230ef012
Auzair-17 <auzair09270@gmail.com>
2026-03-29 11:03:36 +0430

    Footer section completed

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Footer.css";
+import footerLogo from "../../assets/footer_logo.svg";
+import userIcon from "../../assets/user_icon.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footerLogo} alt="" />
+          <p>
+            I am a frontend developer from California, USA with 10 years of
+            experience in multiple companies like Microsoft, Tesla and Apple
+          </p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={userIcon} alt="" />
+            <input type="email" name="" placeholder="Enter your email" />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          &copy; 2026 Alex Bennett. All rights reseved
+        </p>
+        <div className="footer-bottom-right">
+          <p>Terms of services</p>
+          <p>Privacy policy</p>
+          <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
